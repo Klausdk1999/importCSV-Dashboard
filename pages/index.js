009d6dcab6398b2f3d7b78d031b10c38a15f40b8
Klausdk1999 <klausdk1999@gmail.com>
2022-12-02 09:51:30 -0300

    show received data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import UseCSV from "@usecsv/react";
+import useSWR from 'swr';
+import { Stack } from '@mui/system';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
+  const { data, error } = useSWR(
+    "/api/data",
+    fetcher
+  );
+  
+  
+  //if (error) return <div>Failed to load data</div>
+  //if (!data) return ();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Example UseCSV Project</title>
       </Head>
-
+      {data ? 
+       ( 
+        <div> 
+          {data} 
+        </div> )
+        : 
+        (
+        <Stack direction="column" justifyContent="center" alignItems="center" spacing={3}> 
+          Loading ...
+        </Stack>
+        )
+      }
+      
       <main className={styles.main}>
         <UseCSV importerKey="852a58b9-44d5-45fd-ae2d-def76722ddb3" user={{ userId: "Klaus" }}>
           Import Data
